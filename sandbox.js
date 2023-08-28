2016bc18ea8822adef9c5a80ef4aeafea6b36ba4
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-08-28 12:35:14 +0530

    how to make the fuction safe by adding type to the parameter

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,27 @@
-var Character = 'luisa';
-console.log(Character);
-var input = document.querySelectorAll('input');
-console.log(input);
-input.forEach(function (input) {
-    console.log(input);
-});
+// const Character ='luisa';
+// console.log(Character);
+// const input = document.querySelectorAll('input');
+// console.log(input);
+// input.forEach(input => {
+//     console.log(input)
+// })
+var Character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// if you hiv er on that you will see the type and it cannot be changed later on
+//Character = 30; // error
+//age ='name' // error
+Character = "admin"; // it will work
+age = 50; // it will work
+// use of fun 
+var circ = function (diameter) {
+    return diameter * 2;
+};
+console.log(circ(20));
+console.log(circ('Hello')); // alows to compile ans will be not a number nana
+// defining the type to safe from error
+var circs = function (diameter) {
+    return diameter * 2;
+};
+console.log(circs(20));
+//console.log(circs('Hello'));// will throw error on compli time as diameter is defined
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,39 @@
-const Character ='luisa';
-console.log(Character);
+// const Character ='luisa';
+// console.log(Character);
 
-const input = document.querySelectorAll('input');
-console.log(input);
+// const input = document.querySelectorAll('input');
+// console.log(input);
 
-input.forEach(input => {
-    console.log(input)
-})
+// input.forEach(input => {
+//     console.log(input)
+// })
+
+let Character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// if you hiv er on that you will see the type and it cannot be changed later on
+//Character = 30; // error
+//age ='name' // error
+Character = "admin" // it will work
+age =50 // it will work
+
+
+
+// use of fun 
+const circ = (diameter)=>{
+    return diameter*2;
+}
+
+console.log(circ(20));
+console.log(circ('Hello')); // alows to compile ans will be not a number nana
+
+// defining the type to safe from error
+const circs = (diameter:number)=>{
+    return diameter*2;
+}
+
+console.log(circs(20));
+//console.log(circs('Hello'));// will throw error on compli time as diameter is defined
+
+
